1b16539f79958309ba10244a0ad8e3623e3fa8ff
Andy <andresdameson@gmail.com>
2018-05-01 14:09:59 -0300

    Organize all module files inside a folder

--- modules/mercadopago/index.js
+++ modules/mercadopago/index.js
@@ -8,22 +8,21 @@ module.exports = function nuxtMercadoPago (_moduleOptions) {
       moduleOptions.public_key ||
       null
   }
-
   // Don't include when no public key is provided
   if(!currentOptions.public_key) {
-    console.log('!currentOptions')
-	  return
-	}
+    return
+  }
 
-	// Add MercadoPago script to head
-	this.options.head.script.push({
-	  src: ('https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js'),
-	  async: true
-	})
+  // Add MercadoPago script to head
+  this.options.head.script.push({
+    src: ('https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js'),
+    async: true
+  })
 
-	// Register plugin
+  // Register plugin
   this.addPlugin({
-    src: path.resolve(__dirname, 'mercadopago-plugin.js'),
+    src: path.resolve(__dirname, 'plugin.js'),
+    fileName: 'mercadopago.js',
     ssr: false,
     options: currentOptions
   })
--- modules/mercadopago/plugin.js
+++ modules/mercadopago/plugin.js
@@ -1,7 +1,7 @@
 export default (ctx, inject) => {
 
-	// Create new MercadoPago instance
-	Mercadopago.setPublishableKey('<%= options.public_key %>')
+  // Create new MercadoPago instance
+  Mercadopago.setPublishableKey('<%= options.public_key %>')
 
   // Inject mercadopago to the context as $mercadopago
   inject('mercadopago', Mercadopago)
