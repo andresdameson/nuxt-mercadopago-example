dd1a2cd4a320f0b8524477a8f3080455aedfabb4
Andy <andresdameson@gmail.com>
2018-05-01 14:12:36 -0300

    Add helpers deal with MercadoPago SDK

--- modules/mercadopago/plugin.js
+++ modules/mercadopago/plugin.js
@@ -1,7 +1,12 @@
+import helpers from 'helpers'
+
 export default (ctx, inject) => {
 
   // Create new MercadoPago instance
   Mercadopago.setPublishableKey('<%= options.public_key %>')
+
+  // Add helpers deal with MercadoPago SDK
+  Mercadopago.helpers = helpers
 
   // Inject mercadopago to the context as $mercadopago
   inject('mercadopago', Mercadopago)
